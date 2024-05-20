7d5477361c10fbfa287da47383dc7e9cbba5090c
manuatrejo <148013542+manuatrejo@users.noreply.github.com>
2024-05-20 20:51:13 +0200

    ft_strncmp.c

--- librería/ft_strncmp.c
+++ librería/ft_strncmp.c
@@ -18,10 +18,10 @@ int	ft_strncmp(char *s1, char *s2, size_t n)
 	while (0 < n && *s1 && *s2 && *s1 == *s2)
 	{
 		n--;
-		if (n == 0)
-			return (*s1 - *s2);
 		s1++;
 		s2++;
 	}
+	if (n == 0)
+		return (0);
 	return (*s1 - *s2);
 }
